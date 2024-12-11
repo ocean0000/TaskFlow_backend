708761df53d0b99172a3ed4d8caa3edfb8bc1c46
ocean0000 <duongbui2612@gmail.com>
2024-12-11 19:50:05 +0700

    uri change

--- back_end/api/src/app.module.ts
+++ back_end/api/src/app.module.ts
@@ -5,9 +5,12 @@ import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VideoModule } from './video/video.module';
 
+
+const uri= `mongodb://mongo:RqoldZCBioHUKZEaLrtcZhBHeODcjIaj@autorack.proxy.rlwy.net:22866`
+
 @Module({
   imports: [UserModule,
-    MongooseModule.forRoot('mongodb-production-c3b1.up.railway.app'),
+    MongooseModule.forRoot(uri),
     VideoModule],
   
   controllers: [AppController],
