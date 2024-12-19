9100caf7248551662ae729f76d9deae66c75c1c6
ocean0000 <duongbui2612@gmail.com>
2024-12-19 12:51:28 +0700

    fix server mongo uri

--- back_end/api/src/app.module.ts
+++ back_end/api/src/app.module.ts
@@ -1,28 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { MongooseModule } from '@nestjs/mongoose';
 import { VideoModule } from './video/video.module';
 import { StorageModule } from './storage/storage.module';
-
 import { ProjectModule } from './project/project.module';
 import { EmailModule } from './email/email.module';
-require('dotenv').config()
-
-
-const uri= process.env.MONGO_URI
 
 @Module({
-  imports: [UserModule,
-    MongooseModule.forRoot(uri),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Make ConfigModule globally available
+    }),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    
+    UserModule,
     VideoModule,
     StorageModule,
-   
     ProjectModule,
-   
-    EmailModule],
-  
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
