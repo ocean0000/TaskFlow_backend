fcff84ce260187a10824107b080d067046695bd2
ocean0000 <duongbui2612@gmail.com>
2024-12-19 14:25:11 +0700

    fix

--- back_end/api/src/email/email.module.ts
+++ back_end/api/src/email/email.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, ScheduleModule.forRoot()],
   controllers: [EmailController],
   providers: [EmailService],
 })
